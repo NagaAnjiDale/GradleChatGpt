4d3869bb486c164b1a145a3489eb9c6d64c6385d
Anjidale <dale.anji@gmail.com>
2023-03-07 22:00:02 -0500

    adding assertions

--- src/test/java/com/example/chatgpt/ChatgptApplicationTests.java
+++ src/test/java/com/example/chatgpt/ChatgptApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.chatgpt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class ChatgptApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
